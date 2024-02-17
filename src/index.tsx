81d5c55a8e134e20be1c392eef3bb757290710b6
Jangcccci <jangcccci@gmail.com>
2024-02-17 12:48:50 +0900

    scss 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import App from './App';
-import './index.css';
+import './index.scss';
 
 const container: HTMLElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
